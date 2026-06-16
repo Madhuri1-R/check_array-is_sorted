602ca99568e4c904a4fb2a1643474ac9da990100
Madhuri Ratanala <ratnalamadhuri306@gmail.com>
2026-06-16 21:05:21 +0530

    Update sorted_array.py

--- sorted_array.py
+++ sorted_array.py
@@ -1,6 +1,6 @@
 def is_sorted(arr):
     for i in range(len(arr)-1):
-        if arr[i] > arr[i+1]:
+        if arr[i] >= arr[i+1]:
             return False
     return True
 n = int(input("Enter the number of elements:"))
